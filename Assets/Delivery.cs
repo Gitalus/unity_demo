8578de06fe2729b1b58e2b3e92119a2e0a7aff88
Italo Guajardo <italog.penaloza@gmail.com>
2022-07-11 20:48:08 -0400

    Fix: Fix color problem with new Color32()

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-  bool hasPackage;
+  [SerializeField] Color32 hasPackageColor = new Color32(255, 255, 0, 255);
+  [SerializeField] Color32 noPackageColor = new Color32(255, 255, 255, 255);
   [SerializeField] float destroyDelay = 0.5f;
+
+  bool hasPackage;
+
+  SpriteRenderer spriteRenderer;
+  void Start()
+  {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+  }
   // Receive the information of the colliding object
   void OnCollisionEnter2D(Collision2D other)
   {
@@ -18,11 +27,13 @@ public class Delivery : MonoBehaviour
     {
       hasPackage = true;
       Debug.Log("Packag Picked");
+      spriteRenderer.color = hasPackageColor;
       Destroy(other.gameObject, destroyDelay);
     }
     if (other.tag == "Customer" && hasPackage)
     {
       Debug.Log("Package delivered");
+      spriteRenderer.color = noPackageColor;
       hasPackage = false;
     }
   }

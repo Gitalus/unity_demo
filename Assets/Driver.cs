cd99f7a8de115d4a0984799135756c920714ba84
Italo Guajardo <italog.penaloza@gmail.com>
2022-07-11 22:26:07 -0400

    Add booster and slower speeder

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-  [SerializeField]
-  float steerSpeed = 300f;
-
-  [SerializeField]
-  float moveSpeed = 10f;
-
-  void Start()
-  {
-  }
+  [SerializeField] float steerSpeed = 300f;
+  [SerializeField] float moveSpeed = 10f;
+  [SerializeField] float slowSpeed = 5f;
+  [SerializeField] float boostSpeed = 20f;
 
   void Update()
   {
@@ -24,5 +19,17 @@ public class Driver : MonoBehaviour
     float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
     transform.Rotate(0, 0, -steerAmount);
     transform.Translate(0, moveAmount, 0);
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.tag == "Boost")
+    {
+      moveSpeed = boostSpeed;
+    }
+    if (other.tag == "Stop")
+    {
+      moveSpeed = slowSpeed;
+    }
   }
 }
